8110c2eac2ab0a43207166d6a414918b4a1b1b2b
Emam999 <mdemamshikder1@gmail.com>
2021-12-08 04:50:52 -0600

    4th commit 8.12.21

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -56,6 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
         drawer: Container(
           width: 250,
+
           child: Drawer(
             child: ListView(
               children: [
@@ -75,13 +76,14 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                         Text('Joint Letter', style: TextStyle(
                           fontSize: 30,
-                          color: Colors.purple,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold
                         ),),
                         Row(
                           children: [
                             Text('fd.mominbd@gmail.com', style: TextStyle(
                               fontSize: 14,
-                              color: Colors.purple,
+                              color: Colors.white,
                             ),),
                             Icon(
                               Icons.arrow_drop_down,
@@ -141,120 +143,122 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
 
-        body: Container(
-          child: Column(
-            children: [
-              SizedBox(height: 30,),
-              Container(
-                  width: MediaQuery.of(context).size.width/1.10,
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(2, 2),
-                        blurRadius: 12,
-                        color: Color.fromRGBO(0, 0, 0, 0.16),
-                      )],
-                  ),
-                  child: Center(
-                    child: Card(
-                      shadowColor: Colors.blue,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 17, top: 20, bottom: 20, right: 10),
-                        child: Text(
-                          'বাংলায় ব্যবহৃত ২৮৬টি যুক্তবর্ণ দেওয়া হয়েছে, এর বাইরে কোন যুক্তবর্ণ সম্ভবত বাংলায় প্রচলিত নেই।',
-                          style: TextStyle(fontSize: 14.50,), maxLines: 2, ),
-                      ),
-                    )
-                  )),
-              SizedBox(height: 60,),
-              Center(
-                child: Container(
-                  width: MediaQuery.of(context).size.width/1.3,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.lightBlue,
-                      shape: RoundedRectangleBorder(
-                        side: BorderSide(color: Colors.white)
-                      )
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                SizedBox(height: 30,),
+                Container(
+                    width: MediaQuery.of(context).size.width/1.10,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(2, 2),
+                          blurRadius: 12,
+                          color: Color.fromRGBO(0, 0, 0, 0.16),
+                        )],
                     ),
-                   onPressed: (){
-                   Navigator.push(context,
-                    MaterialPageRoute(builder: (context)=>
-                  FirstPage()));
-            },
-                      child: Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Row(
-                          children: [
-                            Text(
-                              'প্রথম অংশ।',
-                              style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-                            ),
-                            Spacer(),
-                            Icon(Icons.chevron_right, color: Colors.white),
-                          ],
+                    child: Center(
+                      child: Card(
+                        shadowColor: Colors.blue,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 17, top: 20, bottom: 20, right: 10),
+                          child: Text(
+                            'বাংলায় ব্যবহৃত ২৮৬টি যুক্তবর্ণ দেওয়া হয়েছে, এর বাইরে কোন যুক্তবর্ণ সম্ভবত বাংলায় প্রচলিত নেই।',
+                            style: TextStyle(fontSize: 14.50,), maxLines: 2, ),
                         ),
-                      ),
-                  ),
-                ),
-              ),
-              Center(
-                child: Container(
-                  width: MediaQuery.of(context).size.width/1.3,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
+                      )
+                    )),
+                SizedBox(height: 60,),
+                Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width/1.3,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
                         primary: Colors.lightBlue,
                         shape: RoundedRectangleBorder(
-                            side: BorderSide(color: Colors.white)
+                          side: BorderSide(color: Colors.white)
+                        )
+                      ),
+                     onPressed: (){
+                     Navigator.push(context,
+                      MaterialPageRoute(builder: (context)=>
+                    FirstPage()));
+              },
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Row(
+                            children: [
+                              Text(
+                                'প্রথম অংশ।',
+                                style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                              ),
+                              Spacer(),
+                              Icon(Icons.chevron_right, color: Colors.white),
+                            ],
+                          ),
                         ),
                     ),
-                      onPressed: (){
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context)=>
-                                SecondPage()));
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Row(
-                          children: [
-                            Text('দ্বিতীয় অংশ।',style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),),
-                            Spacer(),
-                            Icon(Icons.chevron_right, color: Colors.white),
-                          ],
-                        ),
-                      )),
+                  ),
                 ),
-              ),
-              Center(
-                child: Container(
-                  width: MediaQuery.of(context).size.width/1.3,
-                  child: ElevatedButton(
+                Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width/1.3,
+                    child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
                           primary: Colors.lightBlue,
                           shape: RoundedRectangleBorder(
                               side: BorderSide(color: Colors.white)
-                          )
+                          ),
                       ),
-                      onPressed: (){
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context)=>
-                                ThirdPage()));
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Row(
-                          children: [
-                            Text('তৃতীয় অংশ।',style: TextStyle(
-                                fontSize: 20,fontWeight: FontWeight.bold),),
-                            Spacer(),
-                            Icon(Icons.chevron_right, color: Colors.white),
-                          ],
-                        ),
-                      )),
+                        onPressed: (){
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context)=>
+                                  SecondPage()));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Row(
+                            children: [
+                              Text('দ্বিতীয় অংশ।',style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),),
+                              Spacer(),
+                              Icon(Icons.chevron_right, color: Colors.white),
+                            ],
+                          ),
+                        )),
+                  ),
                 ),
-              ),
-            ],
+                Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width/1.3,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.lightBlue,
+                            shape: RoundedRectangleBorder(
+                                side: BorderSide(color: Colors.white)
+                            )
+                        ),
+                        onPressed: (){
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context)=>
+                                  ThirdPage()));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Row(
+                            children: [
+                              Text('তৃতীয় অংশ।',style: TextStyle(
+                                  fontSize: 20,fontWeight: FontWeight.bold),),
+                              Spacer(),
+                              Icon(Icons.chevron_right, color: Colors.white),
+                            ],
+                          ),
+                        )),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
 
